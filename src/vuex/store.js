a98b9756cd73e271d36b9de57eece5ae1e0a22ea
shangxinbo <314911714@qq.com>
2017-04-01 10:06:19 +0800

    add assets

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,12 +5,36 @@ import Vue from 'vue'
 Vue.use(Vuex)
 
 const state = {
-    test: false
+    showLayer: false
 }
 
-const mutations = {}
+const mutations = {
+    //全屏
+    PAGE_NO_SCROLL(state) {
+        state.wholePage = true
+    },
+    PAGE_WIDTH_SCROLL(state) {
+        state.wholePage = false
+    },
+    // 显示遮罩层
+    SHOW_LAYER(state) {
+        state.showLayer = true
+    },
+    // 隐藏遮罩层
+    HIDE_LAYER(state) {
+        state.showLayer = false
+    },
+    // 关闭所有弹窗
+    CLOSE_DIALOG(state) {
+        state.dialog.crowdInit = false
+        state.dialog.crowdCreate = false
+        state.dialog.crowdSelect = false
+        state.showLayer = false
+    }
+}
 
 export default new Vuex.Store({
+    strict:true,
     state,
     mutations
 })
