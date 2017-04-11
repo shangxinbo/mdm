ecbf40873dc7c563ce05685664cedcab637e147d
shangxinbo <314911714@qq.com>
2017-04-11 14:08:15 +0800

    外呼成功

--- api/callCenter/saveCallUuid.js
+++ api/callCenter/saveCallUuid.js
@@ -1,4 +1,4 @@
-module.export = function () {
+module.exports = function () {
     return {
         "work_number": 6,
         "extend_num": 8001, 
--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,12 @@ const customer_check = resolve => System.import('components/customer/check.vue')
 const myinfo = resolve => System.import('components/users/info.vue')
 const myself = resolve => System.import('components/users/customer_my.vue')
 const myseat = resolve => System.import('components/users/myseat.vue')
-const seat = resolve => System.import('components/seat/index.vue')
 const project_list = resolve => System.import('components/project/index.vue')
 const project_call_list = resolve => System.import('components/project/calllist.vue')
 const project_detail = resolve => System.import('components/project/detail.vue')
 const project_add = resolve => System.import('components/project/add.vue')
 const call_index = resolve => System.import('components/call/index.vue')
 const call_cate = resolve => System.import('components/call/category.vue')
-const outbound = resolve => System.import('components/seat/outbound.vue')
 const expense_project = resolve => System.import('components/expense/project.vue')
 const expense_seat = resolve => System.import('components/expense/seat.vue')
 const expense_balance = resolve => System.import('components/expense/balance.vue')
@@ -46,7 +44,6 @@ let mRouter = new Router({
                 { path: '/user/myinfo', name: 'myinfo', component: myinfo },
                 { path: '/user/myself', name: 'myself', component: myself },
                 { path: '/user/myseat', name: 'myseat', component: myseat },
-                { path: '/seat/index/:page?', name: 'seat', component: seat },
                 { path: '/project/index/', name: 'project_list', component: project_list },
                 { path: '/project/call/', name: 'project_call_list', component: project_call_list },
                 { path: '/project/detail/:id/', name: 'project_detail', component: project_detail },
@@ -56,8 +53,7 @@ let mRouter = new Router({
                 { path: '/expense/project/:page?', name: 'expense_project', component: expense_project },
                 { path: '/expense/seat/:page?', name: 'expense_seat', component: expense_seat },
                 { path: '/expense/balance/:page?', name: 'expense_balance', component: expense_balance },
-                { path: '/expense/doc', name: 'expense_doc', component: expense_doc },
-                { path: '/seat/outbound/:id/', name: 'outbound', component: outbound }
+                { path: '/expense/doc', name: 'expense_doc', component: expense_doc }
             ]
         },
         { path: '/error*', name: 'error', component: error },
