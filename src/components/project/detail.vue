23f1e53e5cd8fb2ca6f0b9d2567df11f0f511134
shangxinbo <314911714@qq.com>
2017-04-17 17:40:59 +0800

    项目详情audit_status

--- src/components/project/detail.vue
+++ src/components/project/detail.vue
@@ -68,7 +68,7 @@
                                 <p class="text red">{{detail.audit_reason}}</p>
                             </div>
                         </li>
-                        <template v-if="detail.status==1&&detail.audit_status>0">
+                        <template v-if="detail.status==1&&detail.audit_status==-2">
                             <li class="both">
                                 <label class="name">线索量</label>
                                 <div class="input-warp">
