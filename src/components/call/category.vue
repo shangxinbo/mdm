8c7bfdc3d21d5ce616fe61107d4e3c757893a2fa
fumengmeng <fumengmeng@geotmt.com>
2017-04-17 15:50:59 +0800

    修改类别

--- src/components/call/category.vue
+++ src/components/call/category.vue
@@ -160,7 +160,7 @@
                     to: new Date(2017, 0, 1),
                     from: new Date()
                 },
-                category : user.type == 3 ? 3 :(this.agent_id ? 1:2),
+                category : user.type == 3 ? 1 :(this.search_agent_id ? 2:3),
                 api: {
                     customerList: API.customer_list_all,
                     agentList: API.customer_type_list,
