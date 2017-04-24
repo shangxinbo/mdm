02a4fdab759c7a946fb30b0bb3f4639c1f8f7afa
fumengmeng <fumengmeng@geotmt.com>
2017-04-24 15:26:22 +0800

    修改excel下载

--- src/services/api.js
+++ src/services/api.js
@@ -64,7 +64,6 @@ export default {
     call_list: API_PRE + '/teltraffic/list',
     call_cate: API_PRE + '/teltraffic/category',
     call_head: API_PRE + '/teltraffic/heads',
-    call_export : API_PRE + '/teltraffic/export',
     get_seat_sign: API_PRE + '/callCenter/getSignInData',
     save_call_uuid: API_PRE + '/callCenter/saveCallUuid',
     add_call_job: API_PRE + '/callCenter/addJob',
