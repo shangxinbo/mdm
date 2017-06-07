f805ca9ea0ffc3cb3a78288fe7c10094fe68450e
shangxinbo <314911714@qq.com>
2017-06-07 11:16:26 +0800

    更改数据源

--- api/teltraffic/category.js
+++ api/teltraffic/category.js
@@ -9,25 +9,17 @@ module.exports = function (req, res) {
             {
                 "id": '@natural',
                 "name": "@ctitle",
-                "client_id":'@natural',
+                "client_id": '@natural',
                 "client_name": "@cname",
                 "call_times": "@int(0,1000)",
-                "effect_call_times" : "@int(0,1000)",
-                 "charge_time" :  {
-                    'hour' : "@int(0,12)",
-                    'day'  : "@int(0,12)",
-                    'min'  : "@int(0,12)",
-                    },
-                "avg_time" : {
-                    'hour' : "@int(0,12)",
-                    'day'  : "@int(0,12)",
-                    'min'  : "@int(0,12)",
-                    },
-                "effect_call_rate" : "@int(0,100)",
-                "uneffect_call_rate" : "@int(0,100)",
-                "uneffect_call_times" : "@int(0,1000)",
-                "seat_num" : "@int(1,10)",
-                "seat_id" : "@int(10000,100000)",
+                "effect_call_times": "@int(0,1000)",
+                "charge_time": "4分钟",
+                "avg_time": "4分钟",
+                "effect_call_rate": "@int(0,100)",
+                "uneffect_call_rate": "@int(0,100)",
+                "uneffect_call_times": "@int(0,1000)",
+                "seat_num": "@int(1,10)",
+                "seat_id": "@int(10000,100000)",
             }
         ]
     })
--- api/teltraffic/heads.js
+++ api/teltraffic/heads.js
@@ -2,18 +2,10 @@ let Mock = require('mockjs')
 let Random = Mock.Random
 module.exports = function (req, res) {
     return Mock.mock({
-            "effect_call_times": 201,
-             "charge_time" :  {
-                    'hour' : "@int(0,12)",
-                    'day'  : "@int(0,12)",
-                    'min'  : "@int(0,12)",
-                    },
-                "avg_time" : {
-                    'hour' : "@int(0,12)",
-                    'day'  : "@int(0,12)",
-                    'min'  : "@int(0,12)",
-                    },
-            "effect_call_rate": 66,
-            "seat_num": 8
+        "effect_call_times": 201,
+        "charge_time": "4分钟",
+        "avg_time": "4分钟",
+        "effect_call_rate": 66,
+        "seat_num": 8
     })
 }
